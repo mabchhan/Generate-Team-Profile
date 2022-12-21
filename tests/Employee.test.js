4dd34121d66d16dacfe20031de4b692fdfbc1e16
Mab <mabchhan@gmail.com>
2022-12-21 14:00:01 -0800

    added test Manager.test.js code

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,35 +3,35 @@ const Employee = require("../lib/Employee");
 // create employee object
 describe("Employee Object", () => {
   it("should return new object", () => {
-    const employeeObj = new Employee();
+    const obj = new Employee();
 
-    expect(typeof employeeObj).toEqual("object");
+    expect(typeof obj).toEqual("object");
   });
 
   // employee name
   it("should return employee name by getName()", () => {
     const name = "Mab chhan";
-    const employeeObj = new Employee(name);
-    expect(employeeObj.getName()).toEqual(name);
+    const obj = new Employee(name);
+    expect(obj.getName()).toEqual(name);
   });
 
   // employee Id
   it("should return employee ID by getId", () => {
     const id = 1;
-    const employeeObj = new Employee("Mab Chhan", id);
-    expect(employeeObj.getId()).toEqual(id);
+    const obj = new Employee("Mab Chhan", id);
+    expect(obj.getId()).toEqual(id);
   });
 
   // employee Email
   it("should return employee Email by getEmail()", () => {
     const email = "chhan@gmail.com";
-    const employeeObj = new Employee("Mab Chhan", 1, email);
-    expect(employeeObj.getEmail()).toEqual(email);
+    const obj = new Employee("Mab Chhan", 1, email);
+    expect(obj.getEmail()).toEqual(email);
   });
 
   // employee role
   it('should return "Employee" getRole()', () => {
-    const employeeObj = new Employee("Mab Chhan", 1, "chhan@gmail.com");
-    expect(employeeObj.getRole()).toEqual("Employee");
+    const obj = new Employee("Mab Chhan", 1, "chhan@gmail.com");
+    expect(obj.getRole()).toEqual("Employee");
   });
 });
